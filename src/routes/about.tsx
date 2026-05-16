9487d3303cecc9e9f15cbd7f585767ca7931d891
gayathri-ambati <gayathriambati444@gmail.com>
2026-05-16 17:11:49 +0530

    gayathri

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -57,7 +57,7 @@ function AboutPage() {
         </div>
       </section> */}
 
-   <MissionVisionPage />
+      <MissionVisionPage />
 
       {/* Timeline */}
       {/* <section className="py-20 bg-gradient-to-b from-white to-[#F8FAFC]">
